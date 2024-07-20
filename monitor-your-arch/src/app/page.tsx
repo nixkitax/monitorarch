aafa331ee7b1db32a456105ede48a754f75598af
Nicol Emanue <nicol.eeemanuele@icloud.com>
2024-07-20 15:51:37 +0200

    Update homepage

--- monitor-your-arch/src/app/page.tsx
+++ monitor-your-arch/src/app/page.tsx
@@ -13,6 +13,18 @@ import {
 } from "@/components/ui/drawer";
 import { Button } from "@/components/ui/button";
 
+import { Card, CardContent } from "@/components/ui/card"
+
+import Autoplay from "embla-carousel-autoplay"
+
+
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel"
 export default function Home() {
 
     const [isVisible, setIsVisible] = useState(false);
@@ -83,15 +95,106 @@ export default function Home() {
     }
 
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-16">
-            <div className="flex flex-row w-full just gap-12">
-                <div className="flex-1 flex items-center justify-center">
-                    <p className="text-5xl font-bold -ml-2.5">Analyze Your Best Friend {localStorage.getItem('userName')}</p>
-                </div>
-                <div className="flex-1 flex items-center justify-center">
-                    <div className="w-96 h-96 bg-gray-200 flex items-center justify-center">
-                        <Mac />
+        <main className="flex min-h-screen flex-col items-center justify-between p-16 ">
+
+            <div className="flex flex-col w-full just gap-12">
+                <div className="flex-1 flex-col items-center justify-center">
+                    <div className="flex-1 flex-col -mx-10 -my-14">
+                        <div>
+                            <p className="font-thin text-xl"> Hello, </p>
+                        </div>
+                        <div>
+                            <p className="font-medium text-xl"> {localStorage.getItem('userName')}</p>
+                        </div>
+                       
                     </div>
+                </div>
+                <div className="flex flex-col">
+                    <div >
+                        <p className="font-medium text-xl pb-3"> What you can do here..</p>
+                    </div>
+                    <div className="flex justify-center">
+                        <Carousel className="w-full ml-24 "
+                            plugins={[
+                                Autoplay({
+                                    delay: 5000,
+                                }),
+                            ]}
+>
+                            <CarouselContent className="-ml-1">
+                                    <CarouselItem key={"paolo"} className="pl-1 md:basis-1/2 lg:basis-1/3">
+                                        <div className="p-1">
+                                            <Card>
+                                                <CardContent className="flex aspect-square items-center justify-center p-6">
+                                                    <span className="text-2xl font-semibold">ciao</span>
+                                                </CardContent>
+                                            </Card>
+                                        </div>
+                                    </CarouselItem>
+                                    <CarouselItem key={"paolo"} className="pl-1 md:basis-1/2 lg:basis-1/3">
+                                        <div className="p-1">
+                                        <Card>
+                                            <CardContent className="flex aspect-square items-center justify-center p-6">
+                                                <span className="text-2xl font-semibold">ciao</span>
+                                            </CardContent>
+                                        </Card>
+                                        </div>
+                                </CarouselItem>
+                                <CarouselItem key={"paolo"} className="pl-1 md:basis-1/2 lg:basis-1/3">
+                                    <div className="p-1">
+                                        <Card>
+                                            <CardContent className="flex aspect-square p-6">
+                                                <div className="flex flex-col">
+                                                    <span className="text-2xl font-semibold">Real-time network chart</span>
+                                                    <p>Ciao</p>
+                                                </div>
+                                            </CardContent>
+                                        </Card>
+                                    </div>
+                                </CarouselItem>
+                                <CarouselItem key={"paolo"} className="pl-1 md:basis-1/2 lg:basis-1/3">
+                                    <div className="p-1">
+                                        <Card>
+                                            <CardContent className="flex aspect-square items-center justify-center p-6">
+                                                <span className="text-2xl font-semibold">ciao</span>
+                                            </CardContent>
+                                        </Card>
+                                    </div>
+                                </CarouselItem>
+
+                            </CarouselContent>
+                            <CarouselPrevious />
+                            <CarouselNext />
+                        </Carousel>
+                    </div>
+                    <div >
+                        <p className="font-medium text-xl pb-3"> What you can do here..</p>
+                    </div>
+                    <div className="flex justify-center">
+                        <Carousel className="w-full ml-24 "
+                            plugins={[
+                                Autoplay({
+                                    delay: 5000,
+                                }),
+                            ]}>
+                            <CarouselContent className="-ml-1">
+                                {Array.from({ length: 5 }).map((_, index) => (
+                                    <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
+                                        <div className="p-1">
+                                            <Card>
+                                                <CardContent className="flex aspect-square items-center justify-center p-6">
+                                                    <span className="text-2xl font-semibold">{index + 1}</span>
+                                                </CardContent>
+                                            </Card>
+                                        </div>
+                                    </CarouselItem>
+                                ))}
+                            </CarouselContent>
+                            <CarouselPrevious />
+                            <CarouselNext />
+                        </Carousel>
+                    </div>
+
                 </div>
             </div>
         </main>
